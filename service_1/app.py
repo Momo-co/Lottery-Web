9af63fe688d2541f6423715006e594616f80d6f7
Suman <suman_grg@outlook.com>
2021-08-11 16:58:37 +0000

    Added other four service

--- service_1/app.py
+++ service_1/app.py
@@ -5,7 +5,20 @@ from application.models import Record
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    colour = requests.get('http://service-2:5000/get/colour').text 
+    number = requests.get('http://service-3:5000/get/num').text 
+
+    lucky_input = {'colour': colour, 'number': number}
+    prize = requests.post('http://service-4:5000/gen.prize', json=lucky_input).json() 
+
+    new_lotto = Record(colour=colour, number=number, prize=prize)
+    db.session.add(new_lotto)
+    db.session.commit()
+
+    all_record = Record.query.all()
+    total_prize = func.sum(Record.prize)
+
+    return render_template("home.html", all_record=all_record, colour=colour, number=number, prize=prize, total_prize=total_prize)
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
