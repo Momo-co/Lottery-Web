eae5d7449dc415dc001ca89444908a4e24c82488
Suman <suman_grg@outlook.com>
2021-08-11 22:25:28 +0000

    Added variables on html

--- service_1/app.py
+++ service_1/app.py
@@ -1,6 +1,10 @@
 from application import app, db
 from flask import redirect, request, render_template, url_for
 from application.models import Record
+import requests
+import datetime
+
+db.create_all()
 
 @app.route('/')
 @app.route('/home')
@@ -9,14 +13,23 @@ def home():
     number = requests.get('http://service-3:5000/get/num').text 
 
     lucky_input = {'colour': colour, 'number': number}
-    prize = requests.post('http://service-4:5000/gen.prize', json=lucky_input).json() 
+    prize = requests.post('http://service-4:5000/gen/prize', json=lucky_input).json() 
 
-    new_lotto = Record(colour=colour, number=number, prize=prize)
+    new_lotto = Record(colour=colour, number=int(number), prize=prize)
     db.session.add(new_lotto)
     db.session.commit()
 
     all_record = Record.query.all()
-    total_prize = func.sum(Record.prize)
+    total_prize = 0
+    for record in all_record:
+        total_prize += record.prize
+    
+    currentDateTime = datetime.datetime.now()
+    day = currentDateTime.strftime("%d")
+    month = currentDateTime.strftime("%B")
+    year = currentDateTime.strftime("%Y")
+    hour = currentDateTime.strftime("%H")
+    minute = currentDateTime.strftime("%M")
 
     return render_template("home.html", all_record=all_record, colour=colour, number=number, prize=prize, total_prize=total_prize)
 
