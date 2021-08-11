95223253aef02f7ba1659b562405325077e2805d
Suman-Gurung <suman_grg@outlook.com>
2021-08-11 13:45:00 +0000

    Added a route

--- service_1/app.py
+++ service_1/app.py
@@ -1,4 +1,11 @@
-from application import app
+from application import app, db
+from flask import redirect, request, render_template, url_for
+from application.models import Record
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
