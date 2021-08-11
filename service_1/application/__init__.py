9c674f79e6d8d71abf76bf604489c2f14807f59f
Suman-Gurung <suman_grg@outlook.com>
2021-08-11 13:47:46 +0000

    Added quotaion marks on my environment variable

--- service_1/application/__init__.py
+++ service_1/application/__init__.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(DATABASE_URI)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = str(os.urandom(16))
 
