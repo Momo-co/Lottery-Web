af10ba705bcf6dd1320d3c5f7e5aaef2fa05ef22
Suman <suman_grg@outlook.com>
2021-08-11 15:53:52 +0000

    Added a function

--- service_2/app.py
+++ service_2/app.py
@@ -3,11 +3,14 @@ import random
 
 app = Flask(__name__)
 
-colour = ['Red', 'Yellow', 'Green']
+colours = ['Red', 'Yellow', 'Green']
+
+def pick_colour(colour_list):
+    return random.choice(colour_list)
 
 @app.route('/get/colour')
 def get_colour():
-    return random.choice(colour)
+    return pick_colour(colours)
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
