5305b8dee3c4ac1f2e38c9e017b4fb70a2d995d8
Suman <suman_grg@outlook.com>
2021-08-11 16:57:10 +0000

    removed a database column

--- service_1/application/models.py
+++ service_1/application/models.py
@@ -4,5 +4,4 @@ class Record(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     colour = db.Column(db.String(10), nullable = False)
     number = db.Column(db.Integer, nullable = False)
-    date = db.Column(db.DateTime, nullable = False)
     prize = db.Column(db.Integer, nullable = False)
