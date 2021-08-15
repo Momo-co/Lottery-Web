9a06093655568127b4ec4c6782b684c97f0a5fce
Suman <suman_grg@outlook.com>
2021-08-15 15:11:33 +0000

    Remove docker login

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker-compose build --parallel && \
-docker login -u ${DOCKERHUB_CREDENTIALS_USR} -p ${DOCKERHUB_CREDENTIALS_PSW}
+docker-compose build --parallel &&
 docker-compose push
