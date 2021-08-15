663c8aa203b363492f46e4eae6a262b7008fd4bc
Suman <suman_grg@outlook.com>
2021-08-15 23:45:58 +0000

    Added ansible installation bash commands

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Install dependencies
+sudo apt update 
+sudo apt install software-properties-common -y
+sudo apt-add-repository --yes --update ppa:ansible/ansible -y
+sudo apt install ansible -y
+
 ansible-playbook -i ansible/inventory.yaml ansible/playbook.yaml
