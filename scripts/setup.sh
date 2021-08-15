5028dfce1e2c250c0666ae68153122f432931aeb
Suman <suman_grg@outlook.com>
2021-08-15 14:57:32 +0000

    Added Y answer command?

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,7 @@ sudo apt-get install curl jq -y
 sudo apt install python3-venv
 sudo apt install python3 -y
 sudo apt install python3-pip
+Y
 
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
