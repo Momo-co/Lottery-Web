f3075924320d9ff2056e726e87cda455a132de0c
Suman <suman_grg@outlook.com>
2021-08-15 15:20:33 +0000

    Added cd ../ command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Get back to previous directory?
+cd ../
+
 # Build Images
-docker-compose build --parallel &&
+docker-compose build --parallel
 
 # Push Image
 docker login -u ${DOCKERHUB_CREDENTIALS_USR} -p ${DOCKERHUB_CREDENTIALS_PSW}
